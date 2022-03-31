cce9e943ded73ce34b9b7e8ff5d8ad893933665e
PaulinhoRDC <pauloricardodiasdacosta@gmail.com>
2022-03-31 18:36:13 +0100

    Ficha 5

---
+++ Fichas/Ficha5/Ex2/TurmaAlunos.java
@@ -0,0 +1,164 @@
+package Fichas.Ficha5.Ex2;
+
+import java.util.*;
+
+public class TurmaAlunos {                                  //ESTRATÉGIA DE COMPOSIÇÃO
+
+    private Map<String, Aluno> alunos;
+    private String nomeTurma;
+    private String codUC;
+
+    //CONSTRUTORES//
+    // i. métodos usuais da classe TurmaAlunos, nomeadamente construtores, getters e setters, equals, toString, clone
+    public TurmaAlunos(){
+        this.alunos = new HashMap<>();
+        //this.alunos = new TreeMap<>();                TAMBÉM PODERIA SER!
+
+        this.nomeTurma = "N/A";
+        this.codUC = "N/A";
+    }
+
+    public TurmaAlunos(Map <String, Aluno> nAlunos, String nNomeTurma, String ncodUC){
+        //setAlunos(nAlunos);
+
+        this.nomeTurma = nNomeTurma;
+        this.codUC = ncodUC;
+
+        this.alunos = new HashMap<>();  //vazio, para agora iterarmos por todos os elementos de nAlunos e colonar as suas informações para dentro deste novo HashMap();
+
+        for(Aluno a: nAlunos.values()){
+            String chave = a.getNumero();
+            this.alunos.put(chave, a.clone());
+        }
+    }
+
+    public TurmaAlunos(TurmaAlunos t){
+        this.alunos = t.getAlunos();
+        this.codUC = t.getCodUC();
+        this.nomeTurma = t.getNomeTurma();
+    }
+
+    //GETTER & SETTER'S//
+    public Map<String, Aluno> getAlunos() {
+        Map<String, Aluno> r = new HashMap<>();
+
+        for(Map.Entry<String, Aluno> e : this.alunos.entrySet()){
+            String chave = e.getKey();
+            Aluno valor = e.getValue();
+
+            r.put(chave, valor.clone());                // r.put ( e.getKey(), e.getValue().clone());
+        }
+
+        return r;
+    }
+
+    public void setAlunos(Map<String, Aluno> nAlunos) {
+        this.alunos = new HashMap<>();
+
+        for(String k : this.alunos.keySet()){
+            this.alunos.put(k, nAlunos.get(k).clone());
+        }
+    }
+
+    public String getNomeTurma() {
+        return nomeTurma;
+    }
+
+    public void setNomeTurma(String nomeTurma) {
+        this.nomeTurma = nomeTurma;
+    }
+
+    public String getCodUC() {
+        return codUC;
+    }
+
+    public void setCodUC(String codUC) {
+        this.codUC = codUC;
+    }
+
+    //OUTROS MÉTODOS QUE TODAS AS CLASSES DEVEM CONTER//
+
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if ((o==null) || (this.getClass() != o.getClass())) return false;
+
+        TurmaAlunos t = (TurmaAlunos) o;
+        return (this.alunos.equals(t.getAlunos())
+                && this.nomeTurma.equals(t.getNomeTurma())
+                && this.codUC.equals(t.getCodUC())
+                );
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Nome da Turma: ").append(this.nomeTurma).append("\n")
+                .append("Codigo da UC: ").append(this.codUC).append("\n");
+
+        for(Aluno a : this.alunos.values()){
+            sb.append(a.toString()).append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    public TurmaAlunos clone(){
+        return new TurmaAlunos(this);
+    }
+
+    //OUTROS MÉTODOS//
+
+    // ii. adicionar um novo aluno à turma, public void insereAluno(Aluno a)
+    public void insereAluno(Aluno a){
+        this.alunos.put(a.getNumero() , a.clone());
+    }
+
+    //iii. dado um código de aluno devolver a instância de Aluno associada, public Aluno getAluno(String codAluno)
+    public Aluno getAluno(String codAluno){
+
+        //this.alunos.get(codAluno).clone();     //assim dava problema, caso não existisse o aluno que procuramos
+        //PRECISAMOS ANTES, DE VERIFICAR SE REALMENTE EXISTE ESTE ALUNO
+
+        if(!this.alunos.containsKey(codAluno)) return null;
+        else return this.alunos.get(codAluno).clone();
+    }
+
+    //iv. remover um aluno dado o seu código, public void removeAluno(String codAluno)
+    public void removeAluno(String codAluno){
+        this.alunos.remove(codAluno);                  //  aqui não precisamos de verificar se existe ou não, para o remove não interessa
+    }
+
+    //v. devolver a informação de todos os números de aluno existentes, public Set<String> todosOsCodigos()
+    public Set<String> todosOsCodigos(){
+        return this.alunos.keySet();                //keySet devolve todas as chaves
+    }
+
+    //vi. devolver a informação de quantos alunos existem na turma, public int qtsAlunos()
+    public int qtsAlunos(){
+        return this.alunos.size();
+    }
+
+    //vii. devolver os alunos ordenados por ordem alfabética do seu nome, public Collection<Aluno> alunosOrdemAlfabetica()
+    public Collection<Aluno> alunosOrdemAlfabetica(){
+        TreeSet<Aluno> t = new TreeSet<>();
+
+        for(Aluno a: this.alunos.values()){             // iterar pelos alunos
+            t.add(a.clone());
+        }
+        return t;
+    }
+
+    //viii. devolver os alunos ordenados por ordem decrescente do seu número, public Set<Aluno> alunosOrdemDescrescenteNumero()
+    //(assume-se que não existem números repetidos, daí ser viável devolver um Set sem correr o risco de a comparação eliminar resultados).
+    public Set<Aluno> alunosOrdemDescrescenteNumero(){
+        Comparator<Aluno> c = (a1,a2) -> a2.getNumero().compareTo(a1.getNumero());
+        //Trocamos a ordem do a2 e a1, para que seja de facto, ordem decrescente.
+
+        TreeSet<Aluno> t = new TreeSet<>(c);
+
+        for(Aluno a: this.alunos.values()){             // iterar pelos alunos
+            t.add(a.clone());
+        }
+        return t;
+    }
+}
---
+++ Fichas/Ficha5/Ex4/GestaoEncomendas.java
@@ -0,0 +1,133 @@
+package Fichas.Ficha5.Ex4;
+
+import Fichas.Ficha5.Ex2.Aluno;
+import Fichas.Ficha5.Ex2.TurmaAlunos;
+
+import java.util.*;
+
+public class GestaoEncomendas {
+
+    private Map<Integer,Encomenda> encomendas;
+
+    public GestaoEncomendas(){
+        this.encomendas = new HashMap<>();
+    }
+
+    public GestaoEncomendas(Map<Integer,Encomenda> nEncomendas){
+        this.encomendas = new HashMap<>();
+
+        for(Encomenda a : nEncomendas.values()){
+            this.encomendas.put(a.getNEnc(), a.clone());
+        }
+    }
+
+    public GestaoEncomendas(GestaoEncomendas e){
+        this.encomendas = e.getEncomendas();
+    }
+
+    public Map<Integer, Encomenda> getEncomendas() {
+        Map<Integer, Encomenda> r = new HashMap<>();
+
+        for(Map.Entry<Integer, Encomenda> e : this.encomendas.entrySet()){
+            Integer chave = e.getKey();
+            Encomenda valor = e.getValue();
+
+            r.put(chave, valor.clone());                // r.put ( e.getKey(), e.getValue().clone());
+        }
+        return r;
+    }
+
+    public void setEncomendas(Map<Integer, Encomenda> e) {
+        this.encomendas = new HashMap<>();
+
+        for(Integer k : this.encomendas.keySet()){
+            this.encomendas.put(k, e.get(k).clone());
+        }
+    }
+
+    //OUTROS MÉTODOS QUE TODAS AS CLASSES DEVEM CONTER//
+
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if ((o==null) || (this.getClass() != o.getClass())) return false;
+
+        GestaoEncomendas t = (GestaoEncomendas) o;
+        return (this.encomendas.equals(t.getEncomendas()));
+    }
+
+    @Override
+    public String toString() {
+        return "GestaoEncomendas{" +
+                "getEncomendas=" + getEncomendas() +
+                ", encomendas=" + encomendas +
+                '}';
+    }
+
+    public GestaoEncomendas clone(){
+        return new GestaoEncomendas(this);
+    }
+
+    //OUTROS MÉTODOS//
+
+    //i. método que determina os códigos de encomenda existentes, public Set<Integer> todosCodigosEnc()
+    public Set<Integer> todosCodigosEnc(){
+        return this.encomendas.keySet();
+    }
+
+    //ii. método que adiciona mais uma encomenda ao sistema, public void addEncomenda(Encomenda enc)
+    public void addEncomenda(Encomenda enc){
+        this.encomendas.put(enc.getNEnc(), enc.clone());
+    }
+
+    //iii. método que dado um código de encomenda devolve a informação respectiva, public Encomenda getEncomenda(Integer codEnc)
+    public Encomenda getEncomenda(Integer codEnc){
+        if(!this.encomendas.containsKey(codEnc)) return null;
+        else{
+            return this.encomendas.get(codEnc).clone();
+        }
+    }
+
+    //iv. método que remove uma encomenda dado o seu código, public void removeEncomenda(Integer codEnc)
+    public void removeEncomenda(Integer codEnc){
+        this.encomendas.remove(codEnc);
+    }
+
+    //v. método que determina a encomenda (identificada pelo código) com mais produtos encomendados, public Integer encomendaComMaisProdutos()
+    public Integer encomendaComMaisProdutos() {
+            Comparator<Encomenda> c = (a1, a2) -> a2.numeroTotalProdutos() - a1.numeroTotalProdutos();
+
+            TreeSet<Encomenda> t = new TreeSet<>(c);
+
+            for(Encomenda e : this.encomendas.values()) {
+                t.add(e); //Nao precisamos de clone porque vamos devolver apenas um inteiro
+            }
+
+            Encomenda r = t.first();
+
+            return r.getNEnc();
+
+            // Usando streams
+            //return this.encomendas.values().stream().sorted(c).map(e -> e.getNEnc()).findFirst().orElse(-1);
+
+    }
+    
+    //vi. método que determina todas as encomendas em que um determinado produto, identificado pelo código, está presente, public Set<Integer> encomendasComProduto(String codProd)
+    public Set<Integer> encomendasComProduto(String codProd){
+
+        return new TreeSet<>();
+    }
+
+
+    /*
+
+        vii. método que determina todas as encomendas com data posterior
+        a uma data fornecida como parâmetro, public
+        Set<Intteger> encomendasAposData(LocalDate d)
+        viii. método que devolve uma ordenação, por ordem decrescente
+        de valor de encomenda, de todas as encomendas do sistema,
+        public Set<Encomenda> encomendasValorDecrescente()
+        ix. método que calcula um map em que associa cada código de
+        produto à lista das encomendas em que foi referida, public
+        Map<String,List<Integer>> encomendasDeProduto()
+     */
+}
